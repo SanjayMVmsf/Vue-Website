fac7d8dcb73cd499a06df042a4dc79a8c494709e
SanjayMVmsf <124862721+SanjayMVmsf@users.noreply.github.com>
2023-03-30 17:45:19 +0530

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,24 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          loader: 'ts-loader',
+          options: {
+            appendTsSuffixTo: [/\.vue$/]
+          }
+        }
+      ]
+    }
   }
-})
+});
